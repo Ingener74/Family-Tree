fb433f110ed899eb175ef6b0c72fb26a03f0a619
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-04-20 00:17:44 +0400

    реформат

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+"""
+Надо попробовать объединить sqlite базу данных
+"""
+
 __author__ = 'Pavel'
+
 
 def main():
     print 'Генеалогическое древо'
     pass
+
 
 if __name__ == '__main__':
     main()
